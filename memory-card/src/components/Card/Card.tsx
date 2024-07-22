284c7d52921c8dce504121b36bb2bcc0bc20c39e
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2024-07-22 20:58:11 +0700

    feat: add card tilt animation

--- memory-card/src/components/Card/Card.tsx
+++ memory-card/src/components/Card/Card.tsx
@@ -1,18 +1,21 @@
 import ICatData from '../../interfaces/CatData/ICatData';
+import Tilt from 'react-parallax-tilt';
 import './CardStyle.scss';
 
 const Card = ({ cat, onClick }: { cat: ICatData; onClick: (catId: string) => void }) => {
   const imageUrl = cat.url;
 
   return (
-    <button
-      className="card"
-      onClick={() => {
-        onClick(cat.id);
-      }}
-    >
-      <img src={imageUrl} alt="cat" />
-    </button>
+    <Tilt className="card-container">
+      <button
+        className="card"
+        onClick={() => {
+          onClick(cat.id);
+        }}
+      >
+        <img src={imageUrl} alt="cat" />
+      </button>
+    </Tilt>
   );
 };
 
