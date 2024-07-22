6b875b2ecfb50194828bc52f8b7cdcaf80b3cb20
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2024-07-22 18:37:16 +0700

    feat: create CardGrid component

---
+++ memory-card/src/components/CardGrid/CardGrid.tsx
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+import Card from '../Card/Card';
+import ICatData from '../../interfaces/CatData/ICatData';
+
+const CardGrid = ({ data }: { data: ICatData[] }) => {
+  const cards = data;
+  const [clickedCards, setClickedCards] = useState<string[]>([]);
+
+  const handleClick = ({ catId: catId }: { catId: string }) => {
+    if (!clickedCards.includes(catId)) {
+      setClickedCards([...clickedCards, catId]);
+    }
+  };
+
+  return (
+    <div className="card-grid">
+      {cards.map((cat) => (
+        <Card key={cat.id} cat={cat} onClick={() => handleClick({ catId: cat.id })} />
+      ))}
+    </div>
+  );
+};
+
+export default CardGrid;
