2f3b767ef52e0f72f05125e60f8c311f6d372ec8
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2024-07-22 20:10:57 +0700

    feat: make layout more responsive

--- memory-card/src/components/Header/Header.tsx
+++ memory-card/src/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import './HeaderStyle.scss';
 const Header = ({ currentScore, highScore }: { currentScore: number; highScore: number }) => {
   return (
     <header>
-      <div>
+      <div className='title'>
         <h1>Cats Memory Game</h1>
         <p>Click on an image to earn points, but don't click on any more than once!</p>
       </div>
