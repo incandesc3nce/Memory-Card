3873be7f00ed94ed6620fe1296d460e6acda9681
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2024-07-22 19:56:51 +0700

    feat: create header element and style

---
+++ memory-card/src/components/Header/Header.tsx
@@ -0,0 +1,18 @@
+import './HeaderStyle.scss';
+
+const Header = ({ currentScore, highScore }: { currentScore: number; highScore: number }) => {
+  return (
+    <header>
+      <div>
+        <h1>Cats Memory Game</h1>
+        <p>Click on an image to earn points, but don't click on any more than once!</p>
+      </div>
+      <div className="score">
+        <p>Current score: {currentScore}</p>
+        <p>High score: {highScore}</p>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
