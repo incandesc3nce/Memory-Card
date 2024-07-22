4fe2e6eb5aa493510ef02c5654730395671085a6
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2024-07-22 18:38:15 +0700

    feat: show card grid with fetched cats

--- memory-card/src/App.tsx
+++ memory-card/src/App.tsx
@@ -1,14 +1,24 @@
-import './App.css'
+import './App.css';
+import { useState, useEffect } from 'react';
+import CardGrid from './components/CardGrid/CardGrid';
+import CAT_URL from './const/CAT_URL';
+import ICatData from './interfaces/CatData/ICatData';
+import { useFetch } from './hooks/useFetch/useFetch';
 
 function App() {
+  const [data, setData] = useState<ICatData[]>([]);
+  const fetchedData: ICatData[] = useFetch<ICatData>(CAT_URL);
 
+  useEffect(() => {
+    setData(fetchedData);
+  }, [fetchedData]);
   return (
     <>
-      <div>
-        hello!
+      <div className="grid-wrapper">
+        <CardGrid data={data} />
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
